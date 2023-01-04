1bc7279f4f996f001703d60e14ada8b739652cd6
Valmir Santana <valmirsantanape@gmail.com>
2023-01-04 19:40:40 -0300

    update

--- collections/ex07_3/src/ExemplosSet.java
+++ collections/ex07_3/src/ExemplosSet.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class ExemplosSet {
      public static void main(String[] args) throws Exception {
@@ -30,5 +32,31 @@ public class ExemplosSet {
         notas.remove(0.0);
 
         System.out.println(notas.toString());
+
+        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+
+        Iterator<Double> iterator_ = notas.iterator();
+        while(iterator_.hasNext()){
+            Double next = iterator_.next();
+            if(next < 7) iterator_.remove();
+        }
+        System.out.println(notas.toString());
+
+        System.out.println("Informe todas as notas na ordem em que elas foram informadas: ");
+        Set<Double> notas2 = new LinkedHashSet<>();
+        notas2.add(7d);
+        notas2.add(8.5);
+        notas2.add(9.3);
+        notas2.add(5d);
+        notas2.add(7d);
+        notas2.add(0d);
+        notas2.add(3.6);
+        
+        
+        System.out.println(notas2);
+
+        System.out.println("Exiba todas as notas em ordem crescente: ");
+        Set<Double> notas3 = new TreeSet<>(notas2);
+        System.out.println(notas3);
     }
 }
